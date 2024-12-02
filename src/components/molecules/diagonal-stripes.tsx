4d2d9279750800ea3322561db8cbd6baf3c82c34
micapareddes <micapareddes@gmail.com>
2024-12-02 20:24:15 -0300

    Adjust diagonal stripes

--- src/components/molecules/diagonal-stripes.tsx
+++ src/components/molecules/diagonal-stripes.tsx
@@ -1,30 +1,25 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 
+type StripeProps = {
+  className: string
+}
+const Stripe = ({className}:StripeProps) => {
+  return (
+    <div 
+          className={cn("absolute xl:flex items-center justify-center gap-16 top-[80%] left-0 w-full transform text-center text-2xl py-3 whitespace-nowrap", className)}
+          >
+          {[...Array(8)].map((_, index) => (
+            <Image key={index} src='/pipefy.svg' alt="Logo da Pipefy" width={75} height={32}/>
+          ))}
+        </div>
+  )
+}
 const DiagonalStripes = () => {
     return (
       <>
-        <div className="hidden absolute h-[82px] xl:flex items-center justify-center gap-16 top-[80%] left-0 w-full transform -rotate-6 text-center text-2xl text-gray-700 bg-white py-2 whitespace-nowrap">
-          {[...Array(8)].map((_, index) => (
-            <Image key={index} src='/pipefy.svg' alt="Logo da Pipefy" width={100} height={100}/>
-          ))}
-        </div>
-        <div className="hidden h-[82px] absolute xl:flex items-center justify-center gap-16 top-[80%] left-0 w-full transform rotate-6 text-center text-2xl text-gray-700 bg-gray-300 py-2 whitespace-nowrap">
-        {[...Array(8)].map((_, index) => (
-            <Image key={index} src='/pipefy.svg' alt="Logo da Pipefy" width={100} height={100}/>
-          ))}
-        </div>
-        <div className="hidden absolute h-[82px] lg:flex xl:hidden items-center justify-center gap-16 top-[90%] left-0 w-full transform text-center text-2xl text-gray-700 bg-gray-300 py-2 whitespace-nowrap">
-            {[...Array(6)].map((_, index) => (
-            <Image key={index} src='/pipefy.svg' alt="Logo da Pipefy" width={100} height={100}/>
-          ))}
-        </div>
-
-        <div className="flex absolute h-[82px] lg:hidden items-center justify-center gap-16 top-[80%] left-0 w-full transform text-center text-2xl text-gray-700 bg-gray-300 py-2 whitespace-nowrap">
-            {[...Array(2)].map((_, index) => (
-            <Image key={index} src='/pipefy.svg' alt="Logo da Pipefy" width={100} height={100}/>
-          ))}
-        </div>
-        
+      <Stripe className="bg-app-blue-100 -rotate-3"/>
+      <Stripe className="bg-app-blue-500 rotate-6"/>
       </>
     );
   };
