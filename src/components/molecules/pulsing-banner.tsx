d5a0df3bce71c71a369cbb1c2a06819eb57dc60b
micapareddes <micapareddes@gmail.com>
2024-12-05 15:14:48 -0300

    Update banner

--- src/components/molecules/pulsing-banner.tsx
+++ src/components/molecules/pulsing-banner.tsx
@@ -2,13 +2,14 @@ import { PulsingDot } from "../atoms/pulsing-dot"
 
 type PulsingDotProps = {
     announcement: string
+    dotColor?: 'green' | 'red'
 }
 
-export const PulsingBanner = ({ announcement }:PulsingDotProps) => {
+export const PulsingBanner = ({ announcement, dotColor='green' }:PulsingDotProps) => {
     return (
         <div className="w-full bg-app-yellow-500">
             <div className="flex gap-2 items-center justify-center py-2 body-small-medium">
-                <PulsingDot />
+                <PulsingDot color={dotColor}/>
                 <p>{ announcement }</p>
             </div>
         </div>
