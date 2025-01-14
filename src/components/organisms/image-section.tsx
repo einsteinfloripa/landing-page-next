1ec431c63787adc8d9b57c055118936c650137f2
VitorValandro <vitormateusd@gmail.com>
2025-01-14 16:43:59 -0300

    fix: adiciona responsividade pra seção de donation

--- src/components/organisms/image-section.tsx
+++ src/components/organisms/image-section.tsx
@@ -20,7 +20,7 @@ export const ImageSection = () => {
             loading="eager" // Load the image eagerly
             sizes="(max-width: 768px) 100vw, 1273px" // Specify different sizes for different viewport widths
           />
-          <div className="absolute -top-8 -right-4 lg:-top-16 lg:right-8 flex items-center justify-center">
+          <div className="absolute -top-8 -right-0 lg:-top-16 lg:right-8 flex items-center justify-center">
             <DonateCta />
           </div>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,129 +1,131 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-		rotate: {
-			'-15': '-15deg',
-		},
-  		colors: {
-			app: {
-				blue: {
-					50: '#E6F3F9',
-					100: '#B0D9EC',
-					200: '#8AC7E2',
-					300: '#54ADD5',
-					500: '#0085C1',
-					600: '#0079B0',
-					900: '#00283A',
-				},
-				violet: {
-					500: '#522781',
-				},
-				yellow: {
-					500: '#FFEB66',
-					900: '#4F4A27'
-				},
-				orange: {
-					400: '#F98167',
-					500: '#F86241',
-				},
-				green: {
-					500: '#54c100',
-				},
-				neutral: {
-					10: '#F5F5F5',
-					50: '#E6E8EC',
-					100: '#B0B7C4',
-					300: '#54647F',
-					600: '#00163A',
-					900: '#000A1B',
-				}
-			},
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-		fontFamily: {
-			roboto: ['var(--font-roboto)', 'sans-serif'],
-			anton: ['var(--font-anton)', 'sans-serif'],
-	    },
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-		maxWidth: {
-			'1440': '90rem'
-		},
-		boxShadow: {
-			'custom': '-36px 74px 23px 0px rgba(28, 28, 28, 0.00), -23px 47px 21px 0px rgba(28, 28, 28, 0.01), -13px 27px 18px 0px rgba(28, 28, 28, 0.05), -6px 12px 13px 0px rgba(28, 28, 28, 0.09), -1px 3px 7px 0px rgba(28, 28, 28, 0.10)',
-		},
-		animation: {
-			'spin-reverse': 'spin-reverse 15s linear infinite',
-			'ripple': 'ripple 3s infinite',
-		  },
-		keyframes: {
-			'spin-reverse': {
-			  from: { transform: 'rotate(0deg)' },
-			  to: { transform: 'rotate(-360deg)' },
-			},
-			'ripple': {
-				'0%': {
-				transform: 'scale(1)',
-				opacity: '0.4',
-				},
-				'100%': {
-					transform: 'scale(2.5)',
-					opacity: '0',
-				},
-			},
-		},
-  	}
+    extend: {
+      rotate: {
+        "-15": "-15deg",
+      },
+      colors: {
+        app: {
+          blue: {
+            50: "#E6F3F9",
+            100: "#B0D9EC",
+            200: "#8AC7E2",
+            300: "#54ADD5",
+            500: "#0085C1",
+            600: "#0079B0",
+            900: "#00283A",
+          },
+          violet: {
+            500: "#522781",
+          },
+          yellow: {
+            500: "#FFEB66",
+            900: "#4F4A27",
+          },
+          orange: {
+            400: "#F98167",
+            500: "#F86241",
+          },
+          green: {
+            500: "#54c100",
+          },
+          neutral: {
+            10: "#F5F5F5",
+            50: "#E6E8EC",
+            100: "#B0B7C4",
+            300: "#54647F",
+            600: "#00163A",
+            900: "#000A1B",
+          },
+        },
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+      },
+      fontFamily: {
+        roboto: ["var(--font-roboto)", "sans-serif"],
+        anton: ["var(--font-anton)", "sans-serif"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      maxWidth: {
+        "1440": "90rem",
+        container: "1360px",
+      },
+      boxShadow: {
+        custom:
+          "-36px 74px 23px 0px rgba(28, 28, 28, 0.00), -23px 47px 21px 0px rgba(28, 28, 28, 0.01), -13px 27px 18px 0px rgba(28, 28, 28, 0.05), -6px 12px 13px 0px rgba(28, 28, 28, 0.09), -1px 3px 7px 0px rgba(28, 28, 28, 0.10)",
+      },
+      animation: {
+        "spin-reverse": "spin-reverse 15s linear infinite",
+        ripple: "ripple 3s infinite",
+      },
+      keyframes: {
+        "spin-reverse": {
+          from: { transform: "rotate(0deg)" },
+          to: { transform: "rotate(-360deg)" },
+        },
+        ripple: {
+          "0%": {
+            transform: "scale(1)",
+            opacity: "0.4",
+          },
+          "100%": {
+            transform: "scale(2.5)",
+            opacity: "0",
+          },
+        },
+      },
+    },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
