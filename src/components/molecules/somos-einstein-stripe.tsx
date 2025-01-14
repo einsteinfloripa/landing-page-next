c0ff04391c656f9c9a3980f05c1c55f50f996754
VitorValandro <vitormateusd@gmail.com>
2025-01-14 16:25:47 -0300

    fix: adiciona responsividade pro stripe

--- src/components/molecules/somos-einstein-stripe.tsx
+++ src/components/molecules/somos-einstein-stripe.tsx
@@ -1,15 +1,15 @@
-export const SomosEinsteinStipe = () => {
+export const SomosEinsteinStripe = () => {
   return (
     <div className="w-full px-auto">
-        <div className="w-full bg-app-blue-100 overflow-hidden py-4 max-w-wrapper">
-        <div className="flex whitespace-nowrap gap-4 text-app-neutral-10 title-3xl">
-            {Array(10)
+      <div className="w-full bg-app-blue-100 py-4">
+        <div className="flex whitespace-nowrap gap-4 overflow-hidden text-app-neutral-10 title-3xl items-center justify-center">
+          {Array(15)
             .fill("Somos Einstein")
             .map((text, index) => (
-                <span key={index}>{text}</span>
+              <span key={index}>{text}</span>
             ))}
         </div>
-        </div>
+      </div>
     </div>
   );
 };
