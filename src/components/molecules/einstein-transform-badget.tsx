7f3c8669b21c49466ed28d4776438137cad90e18
VitorValandro <vitormateusd@gmail.com>
2025-01-14 15:58:11 -0300

    fix: adiciona responsividade pra seção about

--- src/components/molecules/einstein-transform-badget.tsx
+++ src/components/molecules/einstein-transform-badget.tsx
@@ -1,30 +1,28 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 type EinsteinTransformBadgetProps = {
-    className?: string
-}
+  className?: string;
+};
 
-export const EinsteinTransformBadget = ({className}:EinsteinTransformBadgetProps) => {
-    return (
-        <div className={cn(className)}>
-            <div
-                className="h-24 w-24 bg-app-blue-500 rounded-full flex items-center justify-center"
-            >
-                <Image 
-                    alt="Icone branco da cabeça do einstein"
-                    src='/logos/logo-icon.svg' 
-                    width={80} 
-                    height={80}
-                />
-                <Image
-                alt="Icone branco da cabeça do einstein"
-                src='/icons/rounded-text-transform.svg' 
-                width={174} 
-                height={174}
-                className="absolute animate-spin-reverse"
-                />
-            </div>
-        </div>
-    )
-}
+export const EinsteinTransformBadget = ({ className }: EinsteinTransformBadgetProps) => {
+  return (
+    <div className={cn(className)}>
+      <div className="size-18 md:size-24 bg-app-blue-500 rounded-full flex items-center justify-center">
+        <Image
+          alt="Icone branco da cabeça do einstein"
+          src="/logos/logo-icon.svg"
+          width={80}
+          height={80}
+        />
+        <Image
+          alt="Texto"
+          src="/icons/rounded-text-transform.svg"
+          width={174}
+          height={174}
+          className="size-[140px] md:size-[174px] absolute animate-spin-reverse"
+        />
+      </div>
+    </div>
+  );
+};
--- src/components/organisms/hero-section.tsx
+++ src/components/organisms/hero-section.tsx
@@ -9,7 +9,7 @@ export const Hero = () => {
       <section className="relative flex justify-center max-w-wrapper">
         <div className="flex flex-col md:items-center px-4 gap-7 mt-14 md:mt-28">
           <h1 className="title-6xl md:text-center max-w-[720px] mt-28">
-            Junte-se a nos e transforme o futuro de aluno para aluno
+            Junte-se a nós e transforme o futuro de aluno para aluno
           </h1>
           <p className="md:text-center md:max-w-[514px]">
             Cursinho pré-vestibular gratuito para estudantes de baixa renda que sonham com uma
