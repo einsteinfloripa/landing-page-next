ce037a6e66afd24ad074b34c67cf401f28920bb9
VitorValandro <vitormateusd@gmail.com>
2025-08-11 11:24:48 -0300

    fix margins

--- src/components/storyblok/sb-contribution-section.tsx
+++ src/components/storyblok/sb-contribution-section.tsx
@@ -25,12 +25,12 @@ const SbContributionSection = ({ blok }: Blok<StoryblokContributionSection>) => 
   return (
     <section className="relative flex items-center justify-center py-32 xl:py-48 sm:px-10 text-app-neutral-900">
       <PaperTextureBackground className="bg-app-blue-500" version={2} opacity={80} />
-      <div className="relative flex flex-col lg:flex-row bg-white rounded-lg gap-10 py-10 px-5 lg:gap-16 lg:p-14 w-full border border-black">
+      <div className="relative flex flex-col lg:flex-row mx-2 bg-white rounded-lg gap-10 py-10 px-5 lg:gap-16 lg:p-14 w-full border border-black">
         {/* Left Container */}
         <div className="flex flex-col gap-10 flex-shrink-1">
           <h1 className="title-4xl lg:w-3/5">{titulo}</h1>
           {subtitulo && <RichText richText={subtitulo} />}
-          <div className="flex items-center md:w-4/5 gap-4">
+          <div className="flex items-center w-full md:w-4/5 gap-4">
             <Image
               className="hidden md:block rounded-lg h-full border border-black"
               src={getWebpVersionFromSBImage(qrCode.filename!)}
@@ -41,7 +41,7 @@ const SbContributionSection = ({ blok }: Blok<StoryblokContributionSection>) => 
 
             <div className="flex flex-col items-start gap-4 md:gap-0">
               <RichText richText={qrCodeDescricao} />
-              <div className="flex flex-row gap-4">
+              <div className="flex flex-wrap gap-4">
                 <Button className="md:hidden">Copiar chave PIX</Button>
                 <SbButtonNav blok={botaoPix[0]} />
               </div>
